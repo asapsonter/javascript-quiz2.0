1c44ce58b979b7781b5f34fe688b0edf319fdea9
Nathan Rose <fullstackroses@gmail.com>
2021-09-13 00:58:54 -0500

    java:

--- assets/java/script.js
+++ assets/java/script.js
@@ -2,4 +2,23 @@
 var score = 0;
 var questionIndex = 0;
 
+//Query Selector
+const question = document.querySelector('#question');
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
 
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+
+//Question Scoring System
+let currentQuestion = {}
+let acceptingAnswer = true
+let score = 0 
+let questionCounter = 0
+let availableQuestions = []
+
+   let question = [
+       {
+           
+       }
+   ]
