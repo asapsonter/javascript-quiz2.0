ea433274f010d08be404f2dde64acde8f599dcc0
Nathan Rose <fullstackroses@gmail.com>
2021-09-27 22:43:29 -0500

    maybe fix

--- assets/java/end.js
+++ assets/java/end.js
@@ -31,5 +31,5 @@ saveHighScore = e => {
     highScores.splice(5)
 
     localStorage.setItem('highScores', JSON.stringify(highScores))
-    window.location.assign('/end.html')
+    window.location.assign('./end.js')
 }
